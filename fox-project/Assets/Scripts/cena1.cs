528ced8d23fdb7c3990188510d859d4a2528b864
Alysson <3oalysson@gmail.com>
2024-06-01 20:03:09 -0300

    teste

---
+++ fox-project/Assets/Scripts/cena1.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class cena1 : MonoBehaviour
+{
+    public string nextLevelName = "Fase1"; // Nome da próxima fase
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(nextLevelName);
+        }
+    }
+}
+
