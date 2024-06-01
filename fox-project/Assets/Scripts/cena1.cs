6134d5def6a96cbe4770daa10b32e0ad89b924d3
Alysson <3oalysson@gmail.com>
2024-06-01 20:04:33 -0300

    teste

--- fox-project/Assets/Scripts/cena1.cs
+++ fox-project/Assets/Scripts/cena1.cs
@@ -11,7 +11,7 @@ public class cena1 : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Fox"))
         {
             SceneManager.LoadScene(nextLevelName);
         }
